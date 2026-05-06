429fd5160543b4707ccfed5e749229c674b3ffa9
Rayyan Khalid <reyyankhalid535@gmail.com>
2026-05-06 11:44:05 -0700

    update sever.js

--- server/server.js
+++ server/server.js
@@ -55,8 +55,14 @@ app.use("/api/messages", messageRouter);
 // Connect to MONGODB
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+// Export server for VERCEL
+export default server;
 
